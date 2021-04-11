2f0963113979b8b052e5106a50efa356af137389
Anubhav Nigam <68975730+a43nigam@users.noreply.github.com>
2021-04-10 20:11:35 -0500

    Update loan_payment_calculator.java

--- loan_payment_calculator.java
+++ loan_payment_calculator.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 public class loan_payment_calculator {
     public static void main(String[] args) {
 
-        System.out.println("Enter initial loan amount: ");
+        System.out.print("Enter initial loan amount: ");
         Scanner kb = new Scanner(System.in);
         double L = kb.nextDouble();
+        System.out.println();
         
-        System.out.println("Enter annual interest rate (percent): ");
+        System.out.print("Enter annual interest rate (percent): ");
         Scanner kb1 = new Scanner(System.in);
         double R = kb1.nextDouble();
+        System.out.println();
         
-        System.out.println("Enter loan term (months): ");
+        System.out.print("Enter loan term (months): ");
         Scanner kb2 = new Scanner(System.in);
         int n = kb2.nextInt();
+        System.out.println();
 
         R = R / 100;
         double r = (R / 12);
-        double temp1 = L * Math.pow((1 + r), n) * r;
-        double temp2 = Math.pow((1 + r), n) - 1;
+        double num = L * Math.pow((1 + r), n) * r;
+        double denom = Math.pow((1 + r), n) - 1;
 
-        double M = (temp1 / temp2);
+        double M = (num / denom);
         M = Math.round(M * 100.0) / 100.0;
 
-        System.out.println("Monthly payment = $" + M);
+        System.out.print("Monthly payment = $" + M);
     }
 }
